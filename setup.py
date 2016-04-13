3d5b61a80a2511d16722496948579441c9265491
althor880 <althor880@gmail.com>
2016-04-13 17:27:34 -0400

    Finished package setup, added dependancies

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from distutils.core import setup
 setup(
-    name = 'houeshunt',
+    name = 'househunt',
     packages = ['househunt'],
+    install_requires=[
+        'os',
+        'hashlib',
+        'urllib',
+        'urllib2',
+        'StringIO',
+        'csv',
+        'requests',
+        'xmltodict',
+        'tinydb',
+        'datetime',
+    ],
     version = '0.1',
     description = 'Python module to search Redfin and combine with results from the Zillow API',
     author = 'AlThor880',
     author_email = 'althor880@gmail.com',
-    url = 'https://github.com/althor880/house',
-    download_url = 'https://github.com/althor880/house/tarball/0.1',
+    url = 'https://github.com/althor880/househunt',
+    download_url = 'https://github.com/althor880/househunt/tarball/0.1',
     keywords = ['house', 'realty', 'zillow', 'redfin', 'api'],
     classifiers = [],
 )
